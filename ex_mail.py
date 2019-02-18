3cfac71cab9f272f2c9c5112e4d25d54af69f813
gys <tubegys@126.com>
2019-02-18 21:59:37 +0800

    delete mail message

--- ex_mail.py
+++ ex_mail.py
@@ -8,43 +8,7 @@ sender = "tubepython@126.com"
 receiver1 = "tubepython@126.com"
 receiver2 = "Yunsheng.Guan@dellteam.com"
 receivers = [receiver1, receiver2]
-mailtext = """
-import smtplib
-from email.mime.text import MIMEText
-from email.utils import formataddr
 
-# sender = "tubegys@126.com"
-# receiver = "tubegys@126.com"
-sender = "tubepython@126.com"
-receiver1 = "tubepython@126.com"
-receiver2 = "tubegys@126.com"
-receivers = [receiver1, receiver2]
-
-
-def mail():
-    ret = True
-    try:
-        msg = MIMEText("hahahaha", "plain", "utf-8")
-        msg['From'] = formataddr(["gys", sender])
-        msg['To'] = formataddr(["gys_receiver", receiver1])
-        msg['To'] = formataddr(["gys_receiver", receiver2])
-        msg['Subject'] = "1121212"
-
-        server = smtplib.SMTP("smtp.126.com", 25)
-        server.login(sender, "coekie123")
-        server.sendmail(sender, receivers, msg.as_string())
-        server.quit()
-
-    except Exception:
-        ret = False
-    return ret
-
-rets = mail()
-if rets:
-    print("success")
-else:
-    print("fail")
-"""
 
 def mail():
     ret = True
